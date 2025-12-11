4084cc2e7caa39e0f52739e094608843ce4d9f57
Wending.Wang <mao5mao5@126.com>
2025-12-11 14:48:06 +0800

    UI: More details

--- web-ui/src/components/project/ProjectDetails.vue
+++ web-ui/src/components/project/ProjectDetails.vue
@@ -17,137 +17,149 @@
     <h2> {{ $t('error') }} </h2>
     <p> {{ $t('unexpected-error-info-message') }} </p>
   </b-message>
-  <table v-else class="table">
-    <b-loading :is-full-page="false" :active="loading" class="small" />
-    <tbody v-if="!loading">
-      <tr v-if="isPropDisplayed('imagesPreview')">
-        <td class="prop-label">{{ $t('images') }}</td>
-        <td class="prop-content">
-          <list-images-preview :project="project" />
-        </td>
-      </tr>
-      <tr>
-        <td class="prop-label">{{ $t('actions') }}</td>
-        <td class="prop-content">
-          <button class="button is-small" @click="showDetailModal = true">
-            More details
-          </button>
-          <project-actions v-if="canManageProject" :project="project" @update="$emit('update', $event)"
-            @delete="$emit('delete')" />
-        </td>
-      </tr>
-    </tbody>
-  </table>
-  <!-- <cytomine-modal :active.sync="showDetailModal" :title="$t('project-details')" @close="showDetailModal = false">
-    <table class="table is-fullwidth">
-      <tbody>
-        <tr v-if="currentAccount.isDeveloper">
-          <td class="prop-label">{{ $t('id') }}</td>
-          <td class="prop-content">{{ project.id }}</td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('name') }}</td>
-          <td class="prop-content">
-            {{ project.name }}
-          </td>
-        </tr>
-        <tr>
+  <div v-else>
+    <table class="table">
+      <b-loading :is-full-page="false" :active="loading" class="small" />
+      <tbody v-if="!loading">
+        <tr v-if="isPropDisplayed('imagesPreview')">
           <td class="prop-label">{{ $t('images') }}</td>
           <td class="prop-content">
-            <router-link :to="`/project/${project.id}/images`">{{ project.numberOfImages }}</router-link>
+            <list-images-preview :project="project" />
           </td>
         </tr>
         <tr>
-          <td class="prop-label">{{ $t('members') }}</td>
+          <td class="prop-label">{{ $t('actions') }}</td>
           <td class="prop-content">
-            {{ this.members.length }}
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('user-annotations') }}</td>
-          <td class="prop-content">
-            <router-link :to="`/project/${project.id}/annotations?type=user`">
-              {{ project.numberOfAnnotations }}
-            </router-link>
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('reviewed-annotations') }}</td>
-          <td class="prop-content">
-            <router-link :to="`/project/${project.id}/annotations?type=reviewed`">
-              {{ project.numberOfReviewedAnnotations }}
-            </router-link>
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('description') }}</td>
-          <td class="prop-content">
-            <cytomine-description :object="project" :canEdit="canManageProject" />
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('tags') }}</td>
-          <td class="prop-content">
-            <cytomine-tags :object="project" :canEdit="canManageProject" />
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('properties') }}</td>
-          <td class="prop-content">
-            <cytomine-properties :object="project" :canEdit="canManageProject" />
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('attached-files') }}</td>
-          <td class="prop-content">
-            <attached-files :object="project" :canEdit="canManageProject" />
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('ontology') }}</td>
-          <td class="prop-content">
-            <router-link v-if="project.ontology" :to="`/ontology/${project.ontology}`">
-              {{ project.ontologyName }}
-            </router-link>
-            <em v-else>{{ $t('no-ontology') }}</em>
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('created-on') }}</td>
-          <td class="prop-content">
-            {{ Number(project.created) | moment('ll') }}
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('creator') }}</td>
-          <td class="prop-content">
-            <list-usernames :users="[creator]" :onlines="onlines" />
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('representatives') }} ({{ representatives.length }})</td>
-          <td class="prop-content">
-            <list-usernames :users="representatives" :onlines="onlines" />
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('managers') }} ({{ managers.length }})</td>
-          <td class="prop-content">
-            <list-usernames :users="managers" :onlines="onlines" />
-          </td>
-        </tr>
-        <tr>
-          <td class="prop-label">{{ $t('contributors') }} ({{ contributors.length }})</td>
-          <td class="prop-content">
-            <list-usernames :users="contributors" :onlines="onlines" />
+            <button class="button is-small" @click="showDetailModal = true">
+              More details
+            </button>
+            <project-actions v-if="canManageProject" :project="project" @update="$emit('update', $event)"
+              @delete="$emit('delete')" />
           </td>
         </tr>
       </tbody>
     </table>
-    <template #footer>
-      <button class="button" @click="showDetailModal = false">{{ $t('button-close') }}</button>
-    </template>
-  </cytomine-modal> -->
+
+    <div v-if="showDetailModal" class="modal is-active">
+      <div class="modal-background" @click="showDetailModal = false"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Case details</p>
+          <button class="delete" aria-label="close" @click="showDetailModal = false"></button>
+        </header>
+        <section class="modal-card-body">
+          <table class="table is-fullwidth">
+            <tbody>
+              <!-- <tr v-if="currentAccount.isDeveloper">
+                <td class="prop-label">{{ $t('id') }}</td>
+                <td class="prop-content">{{ project.id }}</td>
+              </tr>
+              <tr>
+                <td class="prop-label">{{ $t('name') }}</td>
+                <td class="prop-content">
+                  {{ project.name }}
+                </td>
+              </tr> -->
+              <tr>
+                <td class="prop-label">{{ $t('images') }}</td>
+                <td class="prop-content">
+                  <router-link :to="`/project/${project.id}/images`">{{ project.numberOfImages }}</router-link>
+                </td>
+              </tr>
+              <tr>
+                <td class="prop-label">{{ $t('members') }}</td>
+                <td class="prop-content">
+                  {{ members.length }}
+                </td>
+              </tr>
+              <tr>
+                <td class="prop-label">{{ $t('user-annotations') }}</td>
+                <td class="prop-content">
+                  <router-link :to="`/project/${project.id}/annotations?type=user`">
+                    {{ project.numberOfAnnotations }}
+                  </router-link>
+                </td>
+              </tr>
+              <!-- <tr>
+                <td class="prop-label">{{ $t('reviewed-annotations') }}</td>
+                <td class="prop-content">
+                  <router-link :to="`/project/${project.id}/annotations?type=reviewed`">
+                    {{ project.numberOfReviewedAnnotations }}
+                  </router-link>
+                </td>
+              </tr> -->
+              <tr>
+                <td class="prop-label">{{ $t('description') }}</td>
+                <td class="prop-content">
+                  <cytomine-description :object="project" :canEdit="canManageProject" />
+                </td>
+              </tr>
+              <tr>
+                <td class="prop-label">{{ $t('tags') }}</td>
+                <td class="prop-content">
+                  <cytomine-tags :object="project" :canEdit="canManageProject" />
+                </td>
+              </tr>
+              <!-- <tr>
+                <td class="prop-label">{{ $t('properties') }}</td>
+                <td class="prop-content">
+                  <cytomine-properties :object="project" :canEdit="canManageProject" />
+                </td>
+              </tr> -->
+              <tr>
+                <td class="prop-label">{{ $t('attached-files') }}</td>
+                <td class="prop-content">
+                  <attached-files :object="project" :canEdit="canManageProject" />
+                </td>
+              </tr>
+              <!-- <tr>
+                <td class="prop-label">{{ $t('ontology') }}</td>
+                <td class="prop-content">
+                  <router-link v-if="project.ontology" :to="`/ontology/${project.ontology}`">
+                    {{ project.ontologyName }}
+                  </router-link>
+                  <em v-else>{{ $t('no-ontology') }}</em>
+                </td>
+              </tr> -->
+              <tr>
+                <td class="prop-label">{{ $t('created-on') }}</td>
+                <td class="prop-content">
+                  {{ Number(project.created) | moment('ll') }}
+                </td>
+              </tr>
+              <!-- <tr>
+                <td class="prop-label">{{ $t('creator') }}</td>
+                <td class="prop-content">
+                  <list-usernames :users="[creator]" :onlines="onlines" />
+                </td>
+              </tr>
+              <tr>
+                <td class="prop-label">{{ $t('representatives') }} ({{ representatives.length }})</td>
+                <td class="prop-content">
+                  <list-usernames :users="representatives" :onlines="onlines" />
+                </td>
+              </tr> -->
+              <tr>
+                <td class="prop-label">{{ $t('managers') }} ({{ managers.length }})</td>
+                <td class="prop-content">
+                  <list-usernames :users="managers" :onlines="onlines" />
+                </td>
+              </tr>
+              <tr>
+                <td class="prop-label">{{ $t('contributors') }} ({{ contributors.length }})</td>
+                <td class="prop-content">
+                  <list-usernames :users="contributors" :onlines="onlines" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <footer class="modal-card-foot" style="justify-content: flex-end;">
+          <button class="button" @click="showDetailModal = false">{{ $t('button-close') }}</button>
+        </footer>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -160,7 +172,6 @@ import CytomineDescription from '@/components/description/CytomineDescription';
 import CytomineProperties from '@/components/property/CytomineProperties';
 import CytomineTags from '@/components/tag/CytomineTags';
 import AttachedFiles from '@/components/attached-file/AttachedFiles';
-import CytomineModal from '@/components/utils/CytomineModal';
 
 export default {
   name: 'project-details',
@@ -171,8 +182,7 @@ export default {
     CytomineDescription,
     CytomineProperties,
     CytomineTags,
-    AttachedFiles,
-    CytomineModal
+    AttachedFiles
   },
   props: {
     project: { type: Object },
@@ -183,7 +193,7 @@ export default {
     return {
       loading: true,
       error: false,
-      showDetailModal: true,
+      showDetailModal: false,
 
       creator: null,
       managers: [],
@@ -238,7 +248,8 @@ export default {
         cancelText: this.$t('button-cancel'),
         onConfirm: () => this.$emit('delete')
       });
-    }
+    },
+
   },
   async created() {
     try {
@@ -258,7 +269,9 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import '../../assets/styles/dark-variables.scss';
+
 .table {
   background: none;
   position: relative;
@@ -277,5 +290,43 @@ td.prop-content {
 
 .prop-label {
   font-weight: bold;
+}
+
+.modal-card {
+  max-width: 800px;
+  width: auto;
+  margin: 0 auto;
+}
+
+.modal-card-head {
+  background-color: $dark-bg-secondary;
+  color: $dark-text-primary;
+  border-bottom: 1px solid $dark-border-color;
+}
+
+.modal-card-body {
+  background-color: $dark-bg-primary;
+  color: $dark-text-primary;
+  max-height: 70vh;
+}
+
+.modal-card-foot {
+  background-color: $dark-bg-secondary;
+  border-top: 1px solid $dark-border-color;
+}
+
+.modal-card-title {
+  color: $dark-text-primary;
+}
+
+.modal-card-foot button {
+  background-color: $dark-button-bg;
+  border-color: $dark-button-border;
+  color: $dark-text-primary;
+}
+
+.modal-card-foot button:hover {
+  background-color: $dark-button-hover-bg;
+  border-color: $dark-button-hover-border;
 }
 </style>
